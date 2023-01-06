90417e23b9cfb3464de74b67234b1a74f8f8754e
lukas2485 <83334573+lukas2485@users.noreply.github.com>
2023-01-06 15:46:11 +0100

    Pin Nadel - Pseudoelemnt

--- files/kontakt.php
+++ files/kontakt.php
@@ -23,22 +23,7 @@
     
         <h1>Kontakt</h1>
 
-        <div class="kontaktBlock">
-            <div class="adresse">
-                
-                <div class="pin"><img src="../img/maps-Pin.png" width="20px" height ="auto" alt=""><h3>Hier finden Sie uns</h3></div>
-                <p>Mag. Simone Hausensteiner<br>
-                    Birkenweg 200<br>
-                    4794 Kopfing im Innkreis    
-                </p>
-            
-            </div>
-
-            <div class="adresse-maps">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d393.4457473877879!2d13.659932515004346!3d48.441620573116644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4774644660404995%3A0xedd2dd03b866ed44!2sBirkenweg%20200%2C%204794%20Kopfing%20im%20Innkreis!5e0!3m2!1sde!2sat!4v1672845835642!5m2!1sde!2sat" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-        </div>
-        
+        <div class="test">Test Pseudoelement</div>
 
 
     </div>        
