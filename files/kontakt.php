0ca16d3b09426798db4dda24f58dc7ae3a7b03e2
lukas2485 <83334573+lukas2485@users.noreply.github.com>
2023-01-12 16:58:31 +0100

    Kontaktpage Erster Entwurf
    Bildaten + Grob Entwurf

--- files/kontakt.php
+++ files/kontakt.php
@@ -10,6 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/kontakt.css">
     <title>Hamburger Navbar</title>
 </head>
 
@@ -23,19 +24,21 @@
     
         <h1>Kontakt</h1>
 
-        <div class="kontaktBlock">
-            <div class="adresse">
-                
-                <div class="pin"><img src="../img/maps-Pin.png" width="20px" height ="auto" alt=""><h3>Hier finden Sie uns</h3></div>
-                <p>Mag. Simone Hausensteiner<br>
-                    Birkenweg 200<br>
-                    4794 Kopfing im Innkreis    
-                </p>
-            
+        <div class="kontaktContainer">
+            <div class="kontaktContent">
+                <h3>Kontaktieren Sie uns</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta similique explicabo qui corporis aspernatur, tenetur hic delectus voluptatum.</p>
+                <div class="logoContainer">
+                    <div class="row">
+                        <div class="element">
+                            <img src="../img/kontakt/map-pin-point-icon.png" alt="">
+                        </div>
+                    </div>
+                    <div class="row"></div>
+                </div>
             </div>
-
-            <div class="adresse-maps">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d393.4457473877879!2d13.659932515004346!3d48.441620573116644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4774644660404995%3A0xedd2dd03b866ed44!2sBirkenweg%20200%2C%204794%20Kopfing%20im%20Innkreis!5e0!3m2!1sde!2sat!4v1672845835642!5m2!1sde!2sat" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="maps">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d393.4457473877879!2d13.659932515004346!3d48.441620573116644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4774644660404995%3A0xedd2dd03b866ed44!2sBirkenweg%20200%2C%204794%20Kopfing%20im%20Innkreis!5e0!3m2!1sde!2sat!4v1672845835642!5m2!1sde!2sat" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
         
