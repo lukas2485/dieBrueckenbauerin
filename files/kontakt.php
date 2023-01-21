c8952a76e4e994fcc35329921085c569fdea816e
lukas2485 <83334573+lukas2485@users.noreply.github.com>
2023-01-21 10:41:42 +0100

    Kleinigkeiten

--- files/kontakt.php
+++ files/kontakt.php
@@ -22,8 +22,6 @@
     
     <div class="content">
     
-        <h1>Kontakt</h1>
-
         <div class="kontaktContainer">
             <div class="kontaktContent">
                 <div class="headline">
@@ -57,7 +55,7 @@
                 </div>
             </div>
             <div class="maps">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3147.5715489681206!2d13.657258660609081!3d48.441530686961045!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4774644660404995%3A0xedd2dd03b866ed44!2sBirkenweg%20200%2C%204794%20Kopfing%20im%20Innkreis!5e0!3m2!1sde!2sat!4v1674232669623!5m2!1sde!2sat" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3147.5715489681206!2d13.657258660609081!3d48.441530686961045!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4774644660404995%3A0xedd2dd03b866ed44!2sBirkenweg%20200%2C%204794%20Kopfing%20im%20Innkreis!5e0!3m2!1sde!2sat!4v1674232669623!5m2!1sde!2sat" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
         
