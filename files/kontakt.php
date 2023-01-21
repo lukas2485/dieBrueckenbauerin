416e76cc3d192acd05ce6bb75c82af8d3d1e6b27
lukas2485 <83334573+lukas2485@users.noreply.github.com>
2023-01-21 16:20:47 +0100

    Icons + Box shadow hinzugefügt

--- files/kontakt.php
+++ files/kontakt.php
@@ -21,39 +21,56 @@
 
     
     <div class="content">
-    
+
+        <!-- Kontaktcontainer -->
         <div class="kontaktContainer">
+            <!-- Kontaktblock -->
             <div class="kontaktContent">
                 <div class="headline">
-                    <h3>Kontak&shy;tieren Sie uns</h3>
+                    <h3>Kontaktieren Sie mich</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta similique explicabo qui corporis aspernatur, tenetur hic delectus voluptatum.</p>
                 </div>
+
+                <!-- Subcontainer mit Symbol -->
                 <div class="innerContactContainer">
                         <div class="element">
-                            <img src="../img/kontakt/map-pin-white.png" alt="">
+                            <div class="elementImg">
+                                <img src="../img/kontakt/map-pin-white.png" alt="">
+                            </div>
                             <h4>ADRESSE</h4>
                             <p>Birkenweg 200  Kopfing im Innkreis</p>
                         </div>
 
                         <div class="element">
-                            <img src="../img/kontakt/map-pin-white.png" alt="">
-                            <h4>ADRESSE</h4>
+                            <div class="elementImg">
+                                <img src="../img/kontakt/Handy.png" alt="">
+                            </div>
+                            <h4>Handy</h4>
                             <p>Birkenweg 200  Kopfing im Innkreis</p>
                         </div>
 
                         <div class="element">
-                        <img src="../img/kontakt/map-pin-white.png" alt="">
-                            <h4>ADRESSE</h4>
+                            <div class="elementImg">
+                                <img src="../img/kontakt/Email.png" alt="">
+                            </div>
+                            <h4>Mail</h4>
                             <p>Birkenweg 200  Kopfing im Innkreis</p>
                         </div>
 
                         <div class="element">
-                        <img src="../img/kontakt/map-pin-white.png" alt="">
-                            <h4>ADRESSE</h4>
+                            <div class="elementImg">
+                                <img src="../img/kontakt/Kalender.png" alt="">
+                            </div>
+                            <h4>Termin</h4>
                             <p>Birkenweg 200  Kopfing im Innkreis</p>
                         </div>
                 </div>
             </div>
+
+            <!-- Horizontale Linie -->
+            <hr>
+            
+            <!-- Karte -->
             <div class="maps">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3147.5715489681206!2d13.657258660609081!3d48.441530686961045!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4774644660404995%3A0xedd2dd03b866ed44!2sBirkenweg%20200%2C%204794%20Kopfing%20im%20Innkreis!5e0!3m2!1sde!2sat!4v1674232669623!5m2!1sde!2sat" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
