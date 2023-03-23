8c817db0b14ffe1e17e3f2e3ee2b000080f9385e
lukas2485 <83334573+lukas2485@users.noreply.github.com>
2023-03-23 20:42:29 +0100

    Kreis verkleinern/vergrößern

--- js/kreis_layout.js
+++ js/kreis_layout.js
@@ -1,19 +1,29 @@
 function hoverRot(event) {
 	// Unsichtbares Element auswählen
+  const target = event.currentTarget;
   const parent = event.currentTarget.parentNode;
   const sideCont = parent.parentNode.querySelector('.flick');
+  
   // Füge der sideCont-Klasse das sichtbare Attribut hinzu
   sideCont.classList.add("visible");
   
-  
+  // Kreis verkleinern
+  target.style.height = "140px";
+  target.style.width = "140px";
 }
 
 function hoverRotStop(event){
 	// Unsichtbares Element auswählen
+  const target = event.currentTarget;
   const parent = event.currentTarget.parentNode;
   const sideCont = parent.parentNode.querySelector('.flick');
   // Füge der timeConSide-Klasse das sichtbare Attribut hinzu
   sideCont.classList.remove("visible");
+
+
+  // Kreis verkleinern
+  target.style.height = "160px";
+  target.style.width = "160px";
 }
 
 
