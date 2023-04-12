5f8641a8c521448c60dcbfd2908b67ec17057fa7
lukas2485 <83334573+lukas2485@users.noreply.github.com>
2023-04-12 20:49:55 +0200

    Impressum u Footer - Erster Entwurf

--- files/footer.php
+++ files/footer.php
@@ -1,3 +1,33 @@
 <footer>
+
+    <!-- ContentBOX 1-->
+    <div class="footer">
+            <!-- InnerContent -->
+            <div class="contentInner">
+                <!-- Section 2 Spaltig -->
+                <div class="footerContainer">
+
+                    <div class="footer_item">
+                        <p>
+                            <span>Die Brückenbauerin</span> <br>
+                            Birkenweg 200 <br>
+                            4794 Kopfing im Innkreis <br>
+                            <span>Telefon: </span>+43 681 10 25 39 54 <br>
+                            <span>E-Mail: </span><a href="mailto:info@diebrückenbauerin.com">info@diebrückenbauerin.com</a>
+                        </p>
+                    </div>
+
+                    <div class="footer_item">
+                        <p>
+                            <a href="../files/impressum.php">Impressum</a> <br>
+                            <span>2023 © Copyright Lukas Ertl</span>
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    <!-- Navigation laden -->
     <script src="../js/navigation.js"></script>
 </footer>
---
+++ files/impressum.php
@@ -0,0 +1,54 @@
+<?php  require_once('navbar.php');  ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/sectionLayout.css">
+    <title>Hamburger Navbar</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php  require_once('navbar.php');  ?>
+
+    <!-- Content -->
+    <div class="content">
+
+        <!-- ContentBOX 1-->
+        <div class="contentBox light_section">
+            <!-- InnerContent -->
+            <div class="contentInner">
+                <!-- Section 2 Spaltig -->
+                <div class="introColumn">
+                    <h3>Impressum</h3>
+
+                    <p>Eine unverwechselbare Summe aus der Familie und dem sozialen Umfeld, in dem er aufwächst und lebt und den Erfahrungen, die er in seinem Leben gemacht hat. In dieser Einzigartigkeit können wir uns alle gegenseitig bereichern.
+                    <br><br>
+                    Oft ist es uns aber nicht möglich, diese Besonderheit unseres Gegenübers zu erkennen. Wir sind zu sehr verletzt, als dass es uns in dem Moment möglich wäre, uns in den anderen hineinzuversetzen.
+                    <br><br>    
+                    Durch meine Kompetenz als Mediatorin möchte ich euch gerne dabei unterstützen, einen Weg zu finden, wieder eine Brücke zum anderen aufzubauen, damit ihr euch wieder mehr miteinander verbunden fühlt und Gespräche mit gegenseitiger Wertschätzung möglich sind.
+                    </p>
+
+
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>        
+
+   
+</body>
+
+
+<!-- Footer -->
+<?php require_once('footer.php') ?>
+</html>
