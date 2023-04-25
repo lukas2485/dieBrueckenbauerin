f3a035a71b79fd4d681740d2814bc89fd1f99117
lukas2485 <83334573+lukas2485@users.noreply.github.com>
2023-04-25 21:06:12 +0200

    Tab Icon festlegen

--- files/impressum.php
+++ files/impressum.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../img/Bridge_Icon.ico" type="image/ico">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/sectionLayout.css">
     <title>Hamburger Navbar</title>
