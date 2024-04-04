4ae73e1e91718afb9038606e1b7f14a35f2c8866
lukas2485 <83334573+lukas2485@users.noreply.github.com>
2024-04-04 16:16:02 +0200

    Terminkalender eingefügt - Menüpunkt fehlt noch

---
+++ termin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./img/Bridge_Icon.ico" type="image/ico">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/sectionLayout.css">
+    <title>Hamburger Navbar</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php require_once('navbar.php');  ?>
+
+
+    <!-- Content -->
+    <div class="content">
+
+        <!-- ContentBOX 1-->
+            <!-- Booking Calendar Appointment Scheduling begin -->
+            <iframe src="https://outlook.office365.com/owa/calendar/DieBrckenbauerin1@diebrueckenbauerin.onmicrosoft.com/bookings/" style="border: 0" width="100%" height="1700px" frameborder="0" margin-top: 20px></iframe>
+            <!-- end Booking Calendar Appointment Scheduling -->
+
+
+
+
+
+
+    </div>
+
+
+</body>
+
+
+<!-- Footer -->
+<?php require_once('footer.php') ?>
+
+</html>
